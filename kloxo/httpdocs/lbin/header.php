3f9e91952b348f7db8644b74b02889bd135c12c9
Ben <benhussenet@gmail.com>
2014-11-13 02:03:52 +0000

    header

--- kloxo/httpdocs/lbin/header.php
+++ kloxo/httpdocs/lbin/header.php
@@ -22,7 +22,17 @@ function header_main()
         print("<!-- Feather Theme -->\n");
         print_header_feather();
     }
-
+<!-- start: Mobile Specific -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- end: Mobile Specific -->
+    
+    <!-- start: CSS -->
+    print('<link id="bootstrap-style" href="/htmllib/rh14/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/htmllib/rh14/css/bootstrap-responsive.min.css" rel="stylesheet">
+    <link id="base-style" href="/htmllib/rh14/css/style.css" rel="stylesheet">
+    <link id="base-style-responsive" href="/htmllib/rh14/css/style-responsive.css" rel="stylesheet">');
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800&subset=latin,cyrillic-ext,latin-ext' rel='stylesheet' type='text/css'>
+    <!-- end: CSS -->
     print("</body>\n</html>\n");
 }
 
