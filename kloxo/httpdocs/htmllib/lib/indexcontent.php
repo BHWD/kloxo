cc87183f52f1e951975344c3e48a155b455e7425
Ben <benhussenet@gmail.com>
2014-11-13 14:38:08 +0000

    hhh

--- kloxo/httpdocs/htmllib/lib/indexcontent.php
+++ kloxo/httpdocs/htmllib/lib/indexcontent.php
@@ -75,6 +75,18 @@ if(!$cgi_forgotpwd ){
                     <p>
                         No problem, <a href="#">click here</a> to get a new password.
                     </p>    
+
+                    <div class="login-text">
+                    <div class="ctr"><img src="/img/login/icon.gif" width="64" height="64" alt="security" /></div>
+                    <?=$logfo?>
+                    <a class="forgotpwd" href="javascript:document.forgotpassword.submit()"><font color="black"><u>Forgot Password?</u></a>
+                    <form name="forgotpassword" method="post" action="/login/">
+                        <input type="hidden" name="frm_forgotpwd" value="1" />
+                    </form>
+                    <script> document.loginform.frm_clientname.focus(); </script>
+                </div>
+                <div class="clr"></div>
+            </div>
                 </div><!--/span-->
             </div><!--/row-->
             
@@ -100,17 +112,7 @@ if(!$cgi_forgotpwd ){
                         </div>
                     </form>
                 </div>
-                <div class="login-text">
-                    <div class="ctr"><img src="/img/login/icon.gif" width="64" height="64" alt="security" /></div>
-                    <?=$logfo?>
-                    <a class="forgotpwd" href="javascript:document.forgotpassword.submit()"><font color="black"><u>Forgot Password?</u></a>
-                    <form name="forgotpassword" method="post" action="/login/">
-                        <input type="hidden" name="frm_forgotpwd" value="1" />
-                    </form>
-                    <script> document.loginform.frm_clientname.focus(); </script>
-                </div>
-                <div class="clr"></div>
-            </div>
+                
         </div>
         <div id="break"></div> 
 
