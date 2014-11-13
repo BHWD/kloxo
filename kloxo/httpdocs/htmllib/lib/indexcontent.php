29617e00c2c1b2837cc3326fd8711a1b40081121
Ben <benhussenet@gmail.com>
2014-11-13 02:29:32 +0000

    ggg

--- kloxo/httpdocs/htmllib/lib/indexcontent.php
+++ kloxo/httpdocs/htmllib/lib/indexcontent.php
@@ -47,16 +47,26 @@ if(!$cgi_forgotpwd ){
                         <a href="#"><i class="halflings-icon cog"></i></a>
                     </div>
                     <h2>Login to your account</h2>
-                   <form name="loginform" action="/htmllib/phplib/" onsubmit="encode_url(loginform) ; return fieldcheck(this);" method="post">
-                        <div class="form-block">
-                            <div class="inputlabel">Username</div>
-                            <input name="frm_clientname" type="text" class="inputbox" size="30" />
-                            <div class="inputlabel">Password</div>
-                            <input name="frm_password" type="password" class="passbox" size="30" />
-                            <br />
+                   <form  class="form-horizontal" name="loginform" action="/htmllib/phplib/" onsubmit="encode_url(loginform) ; return fieldcheck(this);" method="post">
+                       <fieldset>
+                            
+                            <div class="input-prepend" title="Username">
+                                <span class="add-on"><i class="halflings-icon user"></i></span>
+                                <input class="input-large span10" name="frm_password" id="username" type="text" placeholder="type username"/>
+                            </div>
+<div class="clearfix"></div>
+
+                            <div class="input-prepend" title="Password">
+                                <span class="add-on"><i class="halflings-icon lock"></i></span>
+                                <input class="input-large span10" name="frm_password" id="password" type="password" placeholder="type password"/>
+                            </div>
+                            <div class="clearfix"></div>
                             <input type="hidden" name="id" value="<?php echo mt_rand() ?>" />
-                            <div align="left"><input type="submit" class="button" name="login" value="Login" /></div>
-                        </div>
+<div class="button-login">  
+                                <button type="submit" class="btn btn-primary">Login</button>
+                            </div>
+
+                       </fieldset>
                     </form>
                     <hr>
                     <h3>Forgot Password?</h3>
