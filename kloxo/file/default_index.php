83791caeb73513c73969b4bf02fdb4c479903916
Ben <benhussenet@gmail.com>
2014-11-13 02:06:22 +0000

    Revert "Addition of CSS assets to default index page"
    This reverts commit 3eadcb1da3bbc4ccd1e2469133c1917ab774a679.

--- kloxo/file/default_index.php
+++ kloxo/file/default_index.php
@@ -21,22 +21,6 @@
 			<meta http-equiv="Content-Type" content="text/html; charset=windows-1252">
 			<link href="/htmllib/css/skin/default/missing-common.css" rel="stylesheet" type="text/css">
 			<link href="/htmllib/lib/admin_login.css" rel="stylesheet" type="text/css">
-
-
-<!-- start: Mobile Specific -->
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<!-- end: Mobile Specific -->
-	
-	<!-- start: CSS -->
-	<link id="bootstrap-style" href="/htmllib/rh14/css/bootstrap.min.css" rel="stylesheet">
-	<link href="/htmllib/rh14/css/bootstrap-responsive.min.css" rel="stylesheet">
-	<link id="base-style" href="/htmllib/rh14/css/style.css" rel="stylesheet">
-	<link id="base-style-responsive" href="/htmllib/rh14/css/style-responsive.css" rel="stylesheet">
-	<link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800&subset=latin,cyrillic-ext,latin-ext' rel='stylesheet' type='text/css'>
-	<!-- end: CSS -->
-
-
-
 	</head>
 <body>
 <table class="header">
