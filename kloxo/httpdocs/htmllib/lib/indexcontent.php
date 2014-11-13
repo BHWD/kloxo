3871e2fb53e4ab71a03d205831459ec44aec4b50
Ben <benhussenet@gmail.com>
2014-11-13 02:17:52 +0000

    login

--- kloxo/httpdocs/htmllib/lib/indexcontent.php
+++ kloxo/httpdocs/htmllib/lib/indexcontent.php
@@ -33,6 +33,53 @@ if(!$cgi_forgotpwd ){
     <link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800&subset=latin,cyrillic-ext,latin-ext' rel='stylesheet' type='text/css'>
     <!-- end: CSS -->
 
+
+
+<div class="container-fluid-full">
+        <div class="row-fluid">
+                    
+            <div class="row-fluid">
+                <div class="login-box">
+                    <div class="icons">
+                        <a href="index.html"><i class="halflings-icon home"></i></a>
+                        <a href="#"><i class="halflings-icon cog"></i></a>
+                    </div>
+                    <h2>Login to your account</h2>
+                    <form class="form-horizontal" action="index.html" method="post">
+                        <fieldset>
+                            
+                            <div class="input-prepend" title="Username">
+                                <span class="add-on"><i class="halflings-icon user"></i></span>
+                                <input class="input-large span10" name="username" id="username" type="text" placeholder="type username"/>
+                            </div>
+                            <div class="clearfix"></div>
+
+                            <div class="input-prepend" title="Password">
+                                <span class="add-on"><i class="halflings-icon lock"></i></span>
+                                <input class="input-large span10" name="password" id="password" type="password" placeholder="type password"/>
+                            </div>
+                            <div class="clearfix"></div>
+                            
+                            <label class="remember" for="remember"><input type="checkbox" id="remember" />Remember me</label>
+
+                            <div class="button-login">  
+                                <button type="submit" class="btn btn-primary">Login</button>
+                            </div>
+                            <div class="clearfix"></div>
+                    </form>
+                    <hr>
+                    <h3>Forgot Password?</h3>
+                    <p>
+                        No problem, <a href="#">click here</a> to get a new password.
+                    </p>    
+                </div><!--/span-->
+            </div><!--/row-->
+            
+
+    </div><!--/.fluid-container-->
+
+    
+
         <!--<div id="ctr" align="center">
             <div class="login">
                 <div class="login-form">
