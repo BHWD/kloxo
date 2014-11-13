37f4199591ad708d1ed66e6689f9649835c07c74
Ben <benhussenet@gmail.com>
2014-11-13 02:20:00 +0000

    f

--- kloxo/httpdocs/htmllib/lib/indexcontent.php
+++ kloxo/httpdocs/htmllib/lib/indexcontent.php
@@ -32,7 +32,9 @@ if(!$cgi_forgotpwd ){
     <link id="base-style-responsive" href="/htmllib/rh14/css/style-responsive.css" rel="stylesheet">
     <link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800&subset=latin,cyrillic-ext,latin-ext' rel='stylesheet' type='text/css'>
     <!-- end: CSS -->
-
+<style type="text/css">
+            body { background: url(/htmllib/rh14/img/bg-login.jpg) !important; }
+        </style>
 
 
 <div class="container-fluid-full">
@@ -78,7 +80,7 @@ if(!$cgi_forgotpwd ){
 
     </div><!--/.fluid-container-->
 
-    
+
 
         <!--<div id="ctr" align="center">
             <div class="login">
